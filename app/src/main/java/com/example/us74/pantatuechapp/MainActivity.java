4da841772ef075012de43a225f0de091aef2ae82
US74 <sourabhkumarjha2196@gmail.com>
2018-01-05 12:23:04 +0530

    fddgdg

--- app/src/main/java/com/example/us74/pantatuechapp/MainActivity.java
+++ app/src/main/java/com/example/us74/pantatuechapp/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.us74.pantatuechapp;
 import android.animation.Animator;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.Image;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -19,24 +22,20 @@ import java.util.TimerTask;
 
 import me.relex.circleindicator.CircleIndicator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
-
-    Animation frombottom;
-    TextView textView;
 
     ImageView imageView;
+
 
     private CircleIndicator circleIndicator;
 
     private ViewPager mpager;
 
+
     //this is for activity
     private int[] layouts = {R.layout.aboutusslider, R.layout.servies, R.layout.client, R.layout.technologies, R.layout.contact_us};
     private mpagerAdapter mpagerAdapter;
-
-
-
 
 
     @Override
@@ -44,57 +43,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //this is for time picker
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new MyTimerTask(),3000,4000);
-
-
-       final TextView textView = (TextView)findViewById(R.id.textabout);
-        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
-        textView.setAnimation(frombottom);
-
-
-
-        //this is for logo design
-        ImageView imageView = (ImageView) findViewById(R.id.ImageView);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.logo);
-        RoundedBitmapDrawable roundedBitmapDrawable= RoundedBitmapDrawableFactory.create(getResources(),bitmap);
-        roundedBitmapDrawable.setCircular(true);
-        imageView.setImageDrawable(roundedBitmapDrawable);
-
-
-
         mpager = (ViewPager) findViewById(R.id.viewPager);
         mpagerAdapter = new mpagerAdapter(layouts, this);
         mpager.setAdapter(mpagerAdapter);
 
 
+        //this is for time picker
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 3000, 8000);
 
 
-        CircleIndicator indicator = (CircleIndicator)findViewById(R.id.ic_demo);
+        //this is for logo design
+        ImageView imageView = (ImageView) findViewById(R.id.ImageView);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        imageView.setImageDrawable(roundedBitmapDrawable);
+
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.ic_demo);
         indicator.setViewPager(mpager);
+
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 
     //this is for time picker
-    public class MyTimerTask extends TimerTask{
+    public class MyTimerTask extends TimerTask {
 
         @Override
         public void run() {
             MainActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    if(mpager.getCurrentItem()==0){
+                    if (mpager.getCurrentItem() == 0) {
                         mpager.setCurrentItem(1);
-                    }else if (mpager.getCurrentItem()==1){
+                    } else if (mpager.getCurrentItem() == 1) {
                         mpager.setCurrentItem(2);
-                    }else if (mpager.getCurrentItem()==2) {
+                    } else if (mpager.getCurrentItem() == 2) {
                         mpager.setCurrentItem(3);
-                    }else if (mpager.getCurrentItem()==3){
+                    } else if (mpager.getCurrentItem() == 3) {
                         mpager.setCurrentItem(4);
 
-                        }else{
+                    } else {
                         mpager.setCurrentItem(0);
-
 
 
                     }
@@ -103,3 +109,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+
+
+
